777859887a777cf69e6be64bb484f47a24597023
DIZZYAYMAN <127598048+DIZZYAYMAN@users.noreply.github.com>
2023-03-23 01:25:04 +0000

    Update script.js

--- module-4/js/script.js
+++ module-4/js/script.js
@@ -5,11 +5,11 @@
     for (var i = 0; i < names.length; i++) {
     
       var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
+      if (firstLetter !== 'j') {
+          helloSpeaker.speak(names[i]);
       } 
       else {
-        helloSpeaker.speak(names[i]);
+         byeSpeaker.speak(names[i]);
       }
     }
     
